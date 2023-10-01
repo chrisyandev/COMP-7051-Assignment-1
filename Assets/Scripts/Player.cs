17ca4623c25b2eb27adc2f21dc20adb55c74f1c8
Parth Chaturvedi <parthcv0101@gmail.com>
2023-10-01 16:40:01 -0700

    Single Player Input Actions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Math = System.Math;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private InputActionReference m_MoveUpAction;
+    // [SerializeField]
+    // private InputActionReference m_MoveUpAction;
 
-    [SerializeField]
-    private InputActionReference m_MoveDownAction;
+    // [SerializeField]
+    // private InputActionReference m_MoveDownAction;
+
+    private InputActions m_inputActions;
+    private InputAction m_movementAction;
+
+    public float paddleSpeed = 5f;
+
+    private void Awake()
+    {
+        m_inputActions = new InputActions();
+        m_movementAction = m_inputActions.Movement.UpDown;
+    }
     
     private void OnEnable()
     {
-        
+        m_inputActions.Enable();
     }
 
     // Start is called before the first frame update:w
+
+    private void OnDisable()
+    {
+        m_inputActions.Disable();
+    }
 
     void Start()
     {
@@ -26,7 +43,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = m_movementAction.ReadValue<Vector2>();
+        Vector3 position = new Vector3(0, 0, movement.y);
+
+        //Get paddle postion
+        float paddlePos = transform.position.z + position.z;
+        if (paddlePos >= 6.5f || paddlePos <= -6.5f)
+            position = Vector3.zero;        
+
+        //Move paddle
+        transform.Translate(position * paddleSpeed * Time.deltaTime);
     }
 
     void OnMoveUp()
