619ff0e4bebde77210e1b2fb1d947e7dbdd27b4f
Norman Lim <norman.lim2@hotmail.com>
2023-10-01 17:26:16 -0700

    timer complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,15 +7,26 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Canvas m_MenuCanvas;
     [SerializeField]
-    private TextMeshProUGUI m_RoundTimer;
+    private Canvas m_ScoreCanvas;
+    [SerializeField]
+    private Canvas m_TimerCanvas;
 
-    [Space(20)]
+
+    public TMP_Text Player1Score;
+    public TMP_Text Player2Score;
+    public TMP_Text RoundTimer;
+
+
+    //[SerializeField]
+    //private TextMeshProUGUI m_RoundTimer;
+
+    [Space( 20 )]
     [SerializeField]
     private InputActionAsset m_DefaultInputActionAsset;
     [SerializeField]
     private InputActionReference m_StartGameAction;
 
-    [Space(20)]
+    [Space( 20 )]
     [SerializeField]
     private Player m_PlayerScript;
     [SerializeField]
@@ -26,6 +35,9 @@ public class GameManager : MonoBehaviour
     private Ball m_Ball;
 
     private float DEFAULT_GAME_TIME_IN_SECONDS = 60.0f;
+    private float timeLeft;
+    private bool timerOn = false;
+
     private bool isInGame = false;
 
 
@@ -33,7 +45,7 @@ public class GameManager : MonoBehaviour
 
     public static GameManager GetInstance()
     {
-        if (_instance == null)
+        if ( _instance == null )
         {
             _instance = new GameManager();
         }
@@ -49,7 +61,7 @@ public class GameManager : MonoBehaviour
         _instance = this;
 
         // Enable all action maps inside the default action asset.
-        foreach (var actionMap in m_DefaultInputActionAsset.actionMaps)
+        foreach ( var actionMap in m_DefaultInputActionAsset.actionMaps )
         {
             actionMap.Enable();
         }
@@ -60,24 +72,52 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeft = DEFAULT_GAME_TIME_IN_SECONDS;
+        Player1Score.text = "12";
+        Player2Score.text = "69";
+
+        timerOn = true;
+        //RoundTimer.text = "00:12";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( timerOn )
+        {
+            if ( timeLeft > 0 )
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer( timeLeft );
+            }
+            else
+            {
+                Debug.Log( "Time up!" );
+                timeLeft = 0;
+                timerOn = false;
+            }
+        }
     }
 
-    public void SetIsInGame(bool toSet)
+    void updateTimer( float currentTime )
+    {
+        currentTime += 1;
+        float minutes = Mathf.FloorToInt( currentTime / 60 );
+        float seconds = Mathf.FloorToInt( currentTime % 60 );
+
+        RoundTimer.text = string.Format( "{0:00} : {1:00}", minutes, seconds );
+
+    }
+
+    public void SetIsInGame( bool toSet )
     {
         isInGame = toSet;
     }
 
-    public void OnStartGame(InputAction.CallbackContext context)
+    public void OnStartGame( InputAction.CallbackContext context )
     {
         isInGame = true;
-        Debug.Log("Started Game");
+        Debug.Log( "Started Game" );
         this.m_MenuCanvas.enabled = false;
         m_StartGameAction.action.started -= OnStartGame;
         // Reset Game State
@@ -91,5 +131,6 @@ public class GameManager : MonoBehaviour
 
     private void OnDestroy()
     {
+
     }
 }
